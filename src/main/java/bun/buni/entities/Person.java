c767ecc6e2b8492cc72c85596404e6da44564224
Samantha Andrade (SamiBunee) <samibuneely@gmail.com>
2024-06-08 01:51:38 +0200

    Small tweaks to Person entity.
    Added tags for Database column names.

--- src/main/java/bun/buni/entities/Person.java
+++ src/main/java/bun/buni/entities/Person.java
@@ -10,7 +10,16 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "insertion_date")
+    private String insertionDate;
 }
