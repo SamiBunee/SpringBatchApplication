7990e0f0560a6665ffb105c328030a70a15b8295
Samantha Andrade (SamiBunee) <samibuneely@gmail.com>
2024-06-08 00:23:02 +0200

    Added BatchConfiguration
    Added BatchConfiguration

---
+++ src/main/java/bun/buni/batch/configurations/BatchConfiguration.java
@@ -0,0 +1,21 @@
+package bun.buni.batch.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfiguration {
+
+    //Propiedades de la conexión:
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.oj.jdbc.Driver"); //or -> "org.h2.Driver"
+        dataSource.setUrl("jdbc:mysql://localhost:3306/batch_database");
+        dataSource.setUsername("root");
+        dataSource.setPassword("454514");
+        return dataSource;
+    }
+}
