3de23e34240afc7d0a0df70a8d71107771c60404
Samantha Andrade (SamiBunee) <samibuneely@gmail.com>
2024-06-08 01:50:27 +0200

    Implemented Item Steps
    Implemented:
    - ItemDecompressStep
    - ItemProcessorStep
    - ItemWriterStep

--- src/main/java/bun/buni/batch/steps/ItemProcessorStep.java
+++ src/main/java/bun/buni/batch/steps/ItemProcessorStep.java
@@ -1,13 +1,42 @@
 package bun.buni.batch.steps;
 
+import bun.buni.entities.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
 public class ItemProcessorStep implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
+        log.info("[STEP] Starting Item Processing Step ♥ ♥ ♥ ♥ ♥ ♥ ");
+
+        @SuppressWarnings("unchecked")
+        List<Person> personList = (List<Person>) chunkContext.getStepContext()
+                .getStepExecution()
+                .getJobExecution()
+                .getExecutionContext()
+                .get("personList");
+
+        assert personList != null;
+        List<Person> personFinalList = personList.stream().peek(person -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            person.setInsertionDate(formatter.format(LocalDateTime.now()));
+        }).toList();
+
+        chunkContext.getStepContext()
+                    .getStepExecution()
+                    .getJobExecution()
+                    .getExecutionContext()
+                    .put("personList", personFinalList);
+
+        log.info("[STEP] Ending Item Processing Step ♥ ♥ ♥ ♥ ♥ ♥ ");
+        return RepeatStatus.FINISHED;
     }
 }
--- src/main/java/bun/buni/batch/steps/ItemWriterStep.java
+++ src/main/java/bun/buni/batch/steps/ItemWriterStep.java
@@ -1,13 +1,42 @@
 package bun.buni.batch.steps;
 
+import bun.buni.entities.Person;
+import bun.buni.services.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+@Slf4j
 public class ItemWriterStep implements Tasklet {
+
+    @Autowired
+    private PersonService personService;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
+        log.info("[STEP] Starting Writing Step ♥ ♥ ♥ ♥ ♥ ♥ ");
+
+        @SuppressWarnings("unchecked")
+        List<Person> personList = (List<Person>) chunkContext.getStepContext()
+                .getStepExecution()
+                .getJobExecution()
+                .getExecutionContext()
+                .get("personList");
+
+        personList.forEach( person -> {
+            if(person != null) {
+                log.info(person.toString());
+            }
+        });
+
+        personService.saveAll(personList);
+
+        log.info("[STEP] Ending Writing Step ♥ ♥ ♥ ♥ ♥ ♥ ");
+        return RepeatStatus.FINISHED;
     }
 }
